816c4e045a3b487e7396dce413934294f9702988
kengo watanabe <kengo@wdkk.co.jp>
2015-02-24 09:38:10 +0900

    Add GPL Lisence comment to some sources.

--- iSCKit/iSCController.h
+++ iSCKit/iSCController.h
@@ -1,11 +1,21 @@
-//
-//  iSCController.h
-//  iSCKit
-//
-//  Created by Axel Balley on 26/10/08.
-//  Modified by Kengo Watanabe on 01/02/2015.
-//  Copyright 2015 Watanabe-DENKI Inc. Some rights reserved.
-//
+/************************************************************************
+ *
+ * Copyright 2015 Kengo Watanabe <kengo@wdkk.co.jp>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************/
 
 #import <UIKit/UIKit.h>
 #import <MediaPlayer/MediaPlayer.h>
